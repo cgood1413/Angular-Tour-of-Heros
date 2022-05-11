be8128270ca6a3fb290c3c2384b5cb106998ab29
Chance Goodwin <cgood1413@gmail.com>
2022-05-11 16:04:46 -0500

    Display List completion

--- src/app/heros/heros.component.ts
+++ src/app/heros/heros.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heros',
@@ -8,14 +9,16 @@ import { Hero } from '../hero';
 })
 export class HerosComponent implements OnInit {
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  selectedHero?: Hero;
+  heroes = HEROES;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
